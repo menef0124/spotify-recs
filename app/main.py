920d39a86263d854b99accc605740ebfa193c0cc
menef0124 <menefee0124@gmail.com>
2021-07-02 08:34:30 -0500

    Adjusted procfiles

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from flask import redirect
 from flask import render_template
 from flask import request
 from flask import url_for
+import requests
 from werkzeug.exceptions import abort
 import spotipy
 import json
